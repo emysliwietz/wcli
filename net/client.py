39826005e3c985e09f9fa5f6651e517fd3f14fb6
Egidius Mysliwietz <egidius@mysliwietz.de>
2020-10-06 14:13:33 +0200

    Added client/server basics

---
+++ net/client.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+"""TCP/IP client."""
+
+import socket
+
+
+def client_program():
+    """Send data to server."""
+    host = "localhost"  # as both code is running on same pc
+    port = 5665  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != "bye":
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print("Received from server: " + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == "__main__":
+    client_program()
