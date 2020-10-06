248bea8521cd380b12cd0cd04d7a342e58b7c32b
Egidius Mysliwietz <egidius@mysliwietz.de>
2020-10-06 17:20:14 +0200

    Added client/server cli options

--- run.py
+++ run.py
@@ -14,9 +14,38 @@ parser.add_argument(
     help="Make browser visible",
 )
 
+parser.add_argument(
+    "-s",
+    "--server",
+    dest="server",
+    action="store_true",
+    default=False,
+    help="Start in server mode",
+)
+
+parser.add_argument(
+    "-c",
+    "--client",
+    dest="server",
+    action="store_false",
+    default=True,
+    help="Start in client mode (default)",
+)
+
+parser.add_argument(
+    "-p",
+    "--port",
+    dest="port",
+    nargs=1,
+    default=5665,
+    help="Override port (default 5665)",
+)
+
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    from main import whatsapp_web as ww
+    print(args.port[0])
+    pass
+    # from main import whatsapp_web as ww
 
-    ww.main(args.visible)
+    # ww.main(args.visible)
